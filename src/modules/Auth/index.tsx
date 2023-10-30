75c197b654eaf942a5ab55ada397282c40ddf2f1
temarych <teemest06@icloud.com>
2023-10-30 09:42:41 +0200

    Update UI

--- src/modules/Auth/index.tsx
+++ src/modules/Auth/index.tsx
@@ -11,14 +11,20 @@ export const Auth = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
-    <Stack height="100vh" width="100%" p={[0, 3]}>
+    <Stack
+      width  = "100%"
+      height = {['unset', '100vh']}
+      p      = {[0, 3]}
+    >
       <Card
-        elevation = {isMobile ? 0: 1}
+        variant   = "outlined"
         sx        = {{
-          maxWidth: ['unset', '25rem'],
-          margin  : [0, 'auto'],
-          width   : '100%',
-          overflow: 'visible'
+          maxWidth    : ['unset', '25rem'],
+          margin      : [0, 'auto'],
+          borderRadius: isMobile ? 0 : undefined,
+          border      : isMobile ? 0 : undefined,
+          width       : '100%',
+          overflow    : 'visible'
         }}
       >
         <Outlet />
