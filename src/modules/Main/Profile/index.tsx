558e5a49d2d593707573728ba049ba4506af22ba
temarych <teemest06@icloud.com>
2023-11-01 21:25:24 +0200

    Add basic search logic

--- src/modules/Main/Profile/index.tsx
+++ src/modules/Main/Profile/index.tsx
@@ -1,22 +1,32 @@
-import { useState }                      from 'react';
-import { useNavigate }                   from 'react-router-dom';
-import { Card, Stack, Tab }                     from '@mui/material';
-import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { useSelf }                       from '@hooks/useSelf';
-import { IUser }                         from '@typings/user';
-import { useSearchGuidesQuery }          from '@store/api/searchApi';
-import { Loader }                        from '@components/Loader';
-import { UserInfo }                      from '@components/UserInfo';
-import { GuideList }                     from '../components/GuideList';
-import { MainContainer }                 from '../components/MainContainer';
-import { SearchField } from '../components/SearchField';
+import { ChangeEvent, useMemo, useState } from 'react';
+import { debounce }                       from 'lodash';
+import { useNavigate }                    from 'react-router-dom';
+import { Card, Stack, Tab }               from '@mui/material';
+import { TabContext, TabList, TabPanel }  from '@mui/lab';
+import { useSelf }                        from '@hooks/useSelf';
+import { IUser }                          from '@typings/user';
+import { useSearchGuidesQuery }           from '@store/api/searchApi';
+import { Loader }                         from '@components/Loader';
+import { UserInfo }                       from '@components/UserInfo';
+import { GuideList }                      from '../components/GuideList';
+import { MainContainer }                  from '../components/MainContainer';
+import { SearchField }                    from '../components/SearchField';
+import { ListPlaceholder }                from '../components/ListPlaceholder';
 
 export const Profile = () => {
-  const navigate         = useNavigate();
-  const { self }         = useSelf();
-  const user             = self as IUser;
-  const { data: guides } = useSearchGuidesQuery(''); 
-  const [tab, setTab]    = useState<'guides' | 'guidebooks'>('guides');
+  const navigate          = useNavigate();
+  const { self }          = useSelf();
+  const user              = self as IUser;
+  const [query, setQuery] = useState('');
+  const { data: guides }  = useSearchGuidesQuery(query); 
+  const [tab, setTab]     = useState<'guides' | 'guidebooks'>('guides');
+
+  const handleQueryChange = useMemo(
+    () => debounce((event: ChangeEvent<HTMLInputElement>) => {
+      setQuery(event.target.value);
+    }, 200),
+    [],
+  );
 
   if (!guides) return <Loader />;
 
@@ -27,7 +37,7 @@ export const Profile = () => {
       </Card>
 
       <Stack spacing={2}>
-        <SearchField />
+        <SearchField onChange={handleQueryChange} />
 
         <Card variant="outlined">
           <TabContext value={tab}>
@@ -40,10 +50,17 @@ export const Profile = () => {
             </TabList>
 
             <TabPanel value="guides" sx={{ p: 0 }}>
-              <GuideList
-                guides  = {guides}
-                onClick = {id => navigate(`/guide/${id}`)}
-              />
+              {guides.length > 0 ? (
+                <GuideList
+                  guides  = {guides}
+                  onClick = {id => navigate(`/guide/${id}`)}
+                />
+              ) : (
+                <ListPlaceholder
+                  emoji   = "🤷‍♂️"
+                  caption = "Nothing found"
+                />
+              )}
             </TabPanel>
             <TabPanel value="guidebooks" sx={{ p: 0 }}>
               Guidebooks
